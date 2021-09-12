4a34f7e33cae1fee5bd646b23b26b6d7af49b6eb
JPabloPena <38082059+JPabloPena@users.noreply.github.com>
2021-09-12 16:09:17 -0500

    Documentación y parámetros iniciales
    Se documentó el código y ya para ejecutar la aplicación se recibe el parámetro del puerto, pues anteriormente estaba quemado en el código.

--- lab1/server.py
+++ lab1/server.py
@@ -1,33 +1,42 @@
-#import socket
+import sys
 from socket import socket, error
 from threading import Thread
 
+#Clase para conectarse con múltiples clientes al tiempo
 class Client(Thread):
 
+    #Método para indentificar cada cliente que se conecta al servidor
     def __init__(self, conn, addr):
 
-        Thread.__init__(self)
+        Thread.__init__(self) #Crea el hilo para el cliente
 
-        self.conn = conn
-        self.addr = addr
+        self.conn = conn #Guarda la conexión
+        self.addr = addr #Guarda la dirección de la conexión
     
+    #Método que posee toda la lógica de negocio
     def run(self):
+
         while True:
             try:
+                #Recibe los datos enviados del cliente y los descodifica
                 datos = self.conn.recv(1024).decode()
             except error:
                 print("[%s] Error de lectura." % self.name)
                 break
             
-            if datos.startswith("SUMAR"):
+            #Como en la variable datos se recibe una cadena, cada condición verifica que fue lo que envió el usuario. 
+            #En caso de que quiera sumar la variable datos recibiría una cadena como esta: SUMAR15.20
+            #Entonces la condición lo que hace es identificar la operación (SUMAR) y luego separar los números en diferentes variables.
+            #Esto mismo sucede para todas las condiciones.
+            if datos.startswith("SUMAR"): 
                 print(str(self.addr), "envía: " + str(datos))
                 indice = datos.find(".")
-                numero1 = datos[5:indice]
-                numero2 = datos[indice+1:len(datos)]
-                suma = int(numero1) + int(numero2)
-                datos = str(suma)
+                numero1 = datos[5:indice] #Según el ejemplo: 15
+                numero2 = datos[indice+1:len(datos)] #Según el ejemplo: 20
+                suma = int(numero1) + int(numero2) #Realiza la operación
+                datos = str(suma) #Lo convierte en una cadena
                 print("SUMA: " + str(datos))
-                self.conn.send(datos.encode())
+                self.conn.send(datos.encode()) #Lo codifica para poder enviarselo al cliente
 
             elif datos.startswith("RESTAR"):
                 print(str(self.addr), "envía: " + str(datos))
@@ -72,24 +81,38 @@ class Client(Thread):
                 print("Enviando: " + str(datos))
                 self.conn.send(datos.encode())
         
-        self.conn.close()
+        self.conn.close() #Termina la conexión con el cliente
         
+#Método que establece la conexión con múltiples clientes y determina de donde se conectan
 def Main():
-
-    mySocket = socket()
+    
+    mySocket = socket() #Se llama al método socket
     host = '0.0.0.0'
-    port = 3000
+    port = int(parameters())
     #mySocket.bind( ('localhost', 8000) )
     #mySocket.bind( ('172.31.15.122', 3000) )
-    mySocket.bind( (host, port) )
-    mySocket.listen(5)
+    mySocket.bind( (host, port) ) #Escucha la conexión con el cliente
+    mySocket.listen(5) #Define cuantos clientes se pueden conectar al servidor al mismo tiempo
 
     while True:
         conn, addr = mySocket.accept()
         print("Conexión desde: " + str(addr))
 
-        c = Client(conn, addr)
-        c.start()
+        c = Client(conn, addr) #Crea el hilo con el cliente
+        c.start() #Empieza la conexión con el cliente
+
+def parameters():
+    if len(sys.argv) == 2:
+        port = sys.argv[1]
+        print('Port:', port)
+        return port
+    else:
+        port = '3000'
+        print('Se estableción el puerto 3000 por defecto. Si desea usar otro puerto cierre la aplicación y recuerde ingresar:')
+        print('$python3 server.py port')
+        print("Ejemplo: $python3 server.py 3000")
+        return port
+    
 
 if __name__ == '__main__':
     Main()
