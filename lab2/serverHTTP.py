a4eaa23e1259f75d4fecfddcfecc08ff5e61a1c3
JPabloPena <38082059+JPabloPena@users.noreply.github.com>
2021-09-14 11:57:10 -0500

    Documentación

--- lab2/serverHTTP.py
+++ lab2/serverHTTP.py
@@ -1,17 +1,23 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import cgi
 
+#Arreglo donde se guardarán las tareas
 tasklist = ['Task 1', 'Task 2', 'Task 3']
 
 class requestHandler(BaseHTTPRequestHandler):
     
+    #Método GET
     def do_GET(self):
+        
+        #Vista inicial (index)
         if self.path.endswith('/tasklist'):
+            #Headers
             self.send_response(200)
             self.send_header('content-type', 'text/html')
             self.end_headers()
             #self.wfile.write('Hello!'.encode())
-
+            
+            #HTML para la vista del cliente 
             output = ''
             output += '<html><body>'
             output += '<h1>Task List</h1>'
@@ -22,13 +28,17 @@ class requestHandler(BaseHTTPRequestHandler):
                 output += '</br>'
 
             output += '</body></html>'
-            self.wfile.write(output.encode())
-
+            
+            self.wfile.write(output.encode()) #Envía la vista al navegador
+        
+        #Vista donde se crean nuevas tareas
         if self.path.endswith('/new'):
+            #Headers
             self.send_response(200)
             self.send_header('content-type', 'text/html')
             self.end_headers()
 
+            #HTML para la vista del cliente 
             output = ''
             output += '<html><body>'
             output += '<h1>Add New Task</h1>'
@@ -40,14 +50,17 @@ class requestHandler(BaseHTTPRequestHandler):
             
             output += '</body></html>'
 
-            self.wfile.write(output.encode())
+            self.wfile.write(output.encode()) #Envía la vista al navegador
 
+        #Ruta que elimina las tareas
         if self.path.endswith('/remove'):
             listIDPath = self.path.split('/')[2]
+            #Headers
             self.send_response(200)
             self.send_header('content-type', 'text/html')
             self.end_headers()
-
+            
+            #HTML que ejecuta el método POST para eliminar la tarea
             output = ''
             output += '<html><body>'
             output += '<h1>Remove task: %s</h1>' % listIDPath.replace('%20', ' ')
@@ -55,9 +68,13 @@ class requestHandler(BaseHTTPRequestHandler):
             output += '<input type="submit" value="Remove"></form>'
             output += '<a href=/tasklist>Cancel</a>'
             output += '</body></html>'
-            self.wfile.write(output.encode())
+            
+            self.wfile.write(output.encode()) #Envía la vista al navegador
 
+    #Método POST
     def do_POST(self):
+        
+        #Condición que permite crear una tarea
         if self.path.endswith('/new'):
             ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
             pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
@@ -67,27 +84,30 @@ class requestHandler(BaseHTTPRequestHandler):
                 fields = cgi.parse_multipart(self.rfile, pdict)
                 new_task = fields.get('task')
                 tasklist.append(new_task[0])
-
+            
+            #Headers
             self.send_response(301)
             self.send_header('content-type', 'text/html')
             self.send_header('Location', '/tasklist')
             self.end_headers()
-
+        
+        #Condición que permite eliminar una tarea
         if self.path.endswith('/remove'):
             listIDPath = self.path.split('/')[2]
             ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
             if ctype == 'multipart/form-data':
                 list_item = listIDPath.replace('%20', ' ')
                 tasklist.remove(list_item)
-
+            
+            #Headers
             self.send_response(301)
             self.send_header('content-type', 'text/html')
             self.send_header('Location', '/tasklist')
             self.end_headers()
 
-
+#Método que inicia la conexión del servidor
 def main():
-    PORT = 9000
+    PORT = 8000
     server = HTTPServer(('', PORT), requestHandler)
     print('Server running on port', PORT)
     server.serve_forever()
