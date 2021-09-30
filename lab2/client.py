b636d05e0360a6405262784e2e5a04dbd82bbb62
JPabloPena <pablo27-99@hotmail.com>
2021-09-30 15:09:20 -0500

    Documentation

--- lab2/client.py
+++ lab2/client.py
@@ -4,52 +4,55 @@ import http.client
 import sys
 
 mySocket = socket.socket()
+
+#Método que ejecuta toda la lógica del negocio
 def client():
-    server, port = parameters()
+    server, port = parameters() #Valores de la IP y puerto por donde accede el cliente al servidor
     serverPort = server + ':' + port
+
     print('------ Runnning Client Application ------')
     print(' [x] ¿Qué operación desea realizar? Escriba únicamente el número: ')
     print('      1. Sumar \n      2. Restar \n      3. Multiplicar \n      4. Dividir \n      5. Salir de la aplicación')
-    
+
     try:
         while(True):
             operation = input(' >')
             msg = ""
 
-            if operation == '1':
+            if operation == '1': #Suma
                 num1 = input(' [Num 1] >')
                 num2 = input(' [Num 2] >')
                 msg = operation + '|' + num1 + '|' + num2
                 print(' [x] Enviando: ', num1, '+', num2)
 
-            elif operation == '2':
+            elif operation == '2': #Resta
                 num1 = input(' [Num 1] >')
                 num2 = input(' [Num 2] >')
                 msg = operation + '|' + num1 + '|' + num2
                 print(' [x] Enviando: ', num1, '-', num2)
 
-            elif operation == '3':
+            elif operation == '3': #Multiplicación
                 num1 = input(' [Num 1] >')
                 num2 = input(' [Num 2] >')
                 msg = operation + '|' + num1 + '|' + num2
                 print(' [x] Enviando: ', num1, 'x', num2)
 
-            elif operation == '4':
+            elif operation == '4': #División
                 num1 = input(' [Num 1] >')
                 num2 = input(' [Num 2] >')
                 msg = operation + '|' + num1 + '|' + num2
                 print(' [x] Enviando: ', num1, '/', num2)
 
-            elif operation == '5':
+            elif operation == '5': #Salir de la aplicación
                 msg = operation
                 break
 
-            mySocket = requests.post(serverPort, msg)
+            mySocket = requests.post(serverPort, msg) #Envía la petición POST al servidor
             print(' [x] La respuesta es: ', mySocket)
             
         if mySocket != "":
             
-            connection = http.client.HTTPConnection(server, int(port))
+            connection = http.client.HTTPConnection(server, int(port)) #Conexión con el servidor
             #connection = http.client.HTTPConnection("http://localhost", 8000)
             connection.request("GET", '/')
             response = connection.getresponse()
@@ -59,6 +62,7 @@ def client():
     except (IndexError, ValueError):
         print('No especificaste una dirección IP y número del puerto!')
 
+#Método que recibe los parámetros iniciales (ip-servidor y puerto)
 def parameters():
     if len(sys.argv) == 3:
         server = 'http://' + sys.argv[1]
--- lab2/server.py
+++ lab2/server.py
@@ -5,21 +5,22 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 mySocket = socket.socket()
 
+#Clase que maneja las peticiones enviadas por los clientes
 class HandlerConection(BaseHTTPRequestHandler):
-    def _set_response(self):
+    def _set_response(self): #Método de los HEADERS
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.send_header('Location', '/results')
         self.end_headers()
 
-    def do_GET(self):
+    def do_GET(self): #Método GET
         if self.path.endswith('/results'):
             conn, addr = mySocket.accept()
             connection(conn)
             self._set_response()
             self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
-    def do_POST(self):
+    def do_POST(self): #Método POST
         content_length = int(self.headers['Content-Length']) 
         post_data = self.rfile.read(content_length) 
         post_data = str(post_data.decode('utf-8'))
@@ -73,6 +74,7 @@ def connection(conn):
             break
     conn.close()
 
+#Método que recibe los parámetros iniciales (ip-servidor y puerto)
 def parameters():
     if len(sys.argv) == 3:
         server = sys.argv[1]
@@ -88,7 +90,7 @@ def parameters():
 def main():
     try:
         server, port = parameters()
-        server = HTTPServer((server, int(port)), HandlerConection)
+        server = HTTPServer((server, int(port)), HandlerConection) #Establece la conexión del servidor
         print('------ Runnning Server Application ------')
         server.serve_forever()
     except Exception as error:
