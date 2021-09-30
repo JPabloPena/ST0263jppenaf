4e84fdaced91f56d9aa9e95839c256e0b3516357
JPabloPena <pablo27-99@hotmail.com>
2021-09-30 14:27:45 -0500

    Client-Server

---
+++ lab2/client.py
@@ -0,0 +1,75 @@
+import socket		 	 
+import requests
+import http.client
+import sys
+
+mySocket = socket.socket()
+def client():
+    server, port = parameters()
+    serverPort = server + ':' + port
+    try:
+        while(True):
+            operation = input(' >')
+            msg = ""
+
+            if operation == '1':
+                num1 = input(' [Num 1] >')
+                num2 = input(' [Num 2] >')
+                msg = operation + '|' + num1 + '|' + num2
+                print(' [x] Enviando: ', num1, '+', num2)
+
+            elif operation == '2':
+                num1 = input(' [Num 1] >')
+                num2 = input(' [Num 2] >')
+                msg = operation + '|' + num1 + '|' + num2
+                print(' [x] Enviando: ', num1, '-', num2)
+
+            elif operation == '3':
+                num1 = input(' [Num 1] >')
+                num2 = input(' [Num 2] >')
+                msg = operation + '|' + num1 + '|' + num2
+                print(' [x] Enviando: ', num1, 'x', num2)
+
+            elif operation == '4':
+                num1 = input(' [Num 1] >')
+                num2 = input(' [Num 2] >')
+                msg = operation + '|' + num1 + '|' + num2
+                print(' [x] Enviando: ', num1, '/', num2)
+
+            elif operation == '5':
+                msg = operation
+                break
+
+            mySocket = requests.post(serverPort, msg)
+            print(' [x] La respuesta es: ', mySocket)
+            
+        if mySocket != "":
+            
+            connection = http.client.HTTPConnection(server, port)
+            #connection = http.client.HTTPConnection("http://localhost", 8000)
+            connection.request("GET", '/')
+            response = connection.getresponse()
+            print("Status: {} and reason: {}".format(response.status, response.reason))
+            connection.close()
+
+    except (IndexError, ValueError):
+        print('No especificaste una dirección IP y número del puerto!')
+
+def parameters():
+    if len(sys.argv) == 3:
+        server = 'http://' + sys.argv[1]
+        port = sys.argv[2]
+        print(' [x] Server:', server,'\n [x] Port:', port)
+        
+    else:
+        print(' [x] Recuerde ingresar: $python3 client.py ip-sever port')
+        print(' [x] Ejemplo: $python3 client.py 100.26.225.171 8000\n')
+
+    return server, port
+
+if __name__ == '__main__':
+    print('------ Runnning Client Application ------')
+    print(' [x] ¿Qué operación desea realizar? Escriba únicamente el número: ')
+    print('      1. Sumar \n      2. Restar \n      3. Multiplicar \n      4. Dividir \n      5. Salir de la aplicación')
+    client()
+    
---
+++ lab2/server.py
@@ -0,0 +1,99 @@
+import socket
+import sys
+from _thread import *
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+mySocket = socket.socket()
+
+class HandlerConection(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Location', '/results')
+        self.end_headers()
+
+    def do_GET(self):
+        if self.path.endswith('/results'):
+            conn, addr = mySocket.accept()
+            connection(conn)
+            self._set_response()
+            self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) 
+        post_data = self.rfile.read(content_length) 
+        post_data = str(post_data.decode('utf-8'))
+        dataArray = post_data.split('|')
+
+        if dataArray[0] == '1':
+            result = int(dataArray[1]) + int(dataArray[2])
+
+        elif dataArray[0] == '2':
+            result = int(dataArray[1]) - int(dataArray[2])
+        
+        elif dataArray[0] == '3':
+            result = int(dataArray[1]) * int(dataArray[2])
+
+        elif dataArray[0] == '4':
+            result = int(dataArray[1]) / int(dataArray[2])
+        
+        print(result)
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+def connection(conn):
+    while True:
+        
+        data = conn.recv(1024).decode()
+        dataArray = data.split('|')
+
+        if dataArray[0] == '1':
+            result = int(dataArray[1]) + int(dataArray[2])
+            msg = ' [x] El resultado es ' + str(result)
+            conn.send(msg.encode())
+        
+        elif dataArray[0] == '2':
+            result = int(dataArray[1]) - int(dataArray[2])
+            msg = ' [x] El resultado es ' + str(result)
+            conn.send(msg.encode())
+        
+        elif dataArray[0] == '3':
+            result = int(dataArray[1]) * int(dataArray[2])
+            msg = ' [x] El resultado es ' + str(result)
+            conn.send(msg.encode())
+        
+        elif dataArray[0] == '4':
+            result = int(dataArray[1]) / int(dataArray[2])
+            msg = ' [x] El resultado es ' + str(result)
+            conn.send(msg.encode())
+        
+        elif dataArray[0] == '5':
+            msg = ' [x] Hasta luego...'
+            conn.send(msg.encode())
+            break
+    conn.close()
+
+def parameters():
+    if len(sys.argv) == 3:
+        server = sys.argv[1]
+        port = sys.argv[2]
+        print(' [x] Server:', server,'\n [x] Port:', port)
+        
+    else:
+        print(' [x] Recuerde ingresar: $python3 server.py ip-sever port')
+        print(' [x] Ejemplo: $python3 server.py 172.31.56.16 8000\n')
+
+    return server, port
+
+def main():
+    try:
+        server, port = parameters()
+        server = HTTPServer((server, port), HandlerConection)
+        print('------ Runnning Server Application ------')
+        server.serve_forever()
+    except Exception as error:
+        print("Server error")
+        print(error)
+
+if __name__ == '__main__':
+    main()
